9d13a32e6e6ef589dad4b3a6f93c3a2674155646
CHJohnston <cdh.johnston@gmail.com>
2020-07-29 09:45:48 -0500

    ASPNET Project - View One Product

--- ASPNET/Models/IProductRepository.cs
+++ ASPNET/Models/IProductRepository.cs
@@ -8,6 +8,7 @@ namespace ASPNET
 {
     public interface IProductRepository
     {
-        public IEnumerable<Product> GetAllProducts();       
+        public IEnumerable<Product> GetAllProducts();
+        public Product GetProduct(int id);
     }
 }
